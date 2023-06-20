c28bbe04f8bac7b2bcb2a1d173f53416805430e9
Krasi Dev <krasidev@github.com>
2023-06-20 23:36:18 +0300

    Added tracking pixel with google analytics.

--- config/system.php
+++ config/system.php
@@ -7,6 +7,12 @@ return [
     'datetime_format' => env('DATETIME_FORMAT', 'Y-m-d H:i:s'),
 
     /**
+     * Your GA Tracking ID.
+     * https://support.google.com/analytics/answer/1008080
+     */
+    'tracking_id' => env('GOOGLE_ANALYTICS_TRACKING_ID'),
+
+    /**
      * META DATA
      */
     'description' => env('META_DESCRIPTION', 'Description'),
--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -1,6 +1,16 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
+    @if ($tracking_id = config('system.tracking_id'))
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id={{ $tracking_id }}"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '{{ $tracking_id }}');
+    </script>
+    @endif
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
